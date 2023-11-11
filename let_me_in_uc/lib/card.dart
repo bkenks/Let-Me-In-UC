3dd85fb61760db17ddaea6970c3ae48cfec07b42
Brian Kenkel <112359658+kenkelbt@users.noreply.github.com>
2023-11-11 14:49:45 -0500

    Added card screen example

---
+++ let_me_in_uc/lib/card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Card extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Card Screen'),
+      ),
+      body: Container(),
+    );
+  }
+}
